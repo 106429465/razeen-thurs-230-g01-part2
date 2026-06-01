06f0d3d53f5a86f2d026479d4b2d806df72ad626
Zain Khan <contact@zain-khan.dev>
2026-06-01 21:49:17 +1000

    fix(login/index.php, manage/manage.inc, include/header.inc) FIXED DARK MODE/LIGHT MODE

--- login/index.php
+++ login/index.php
@@ -2,11 +2,14 @@
 <?php
 $current_page = 'Manage';
 include '../include/header.inc';
+// Capture the theme from the URL if it exists
+$theme = isset($_GET['theme']) ? $_GET['theme'] : 'light'; 
+$form_action = "validate_login.php?theme=$theme#$theme";
 ?>
 
 <h1>HR Login Portal</h1>
 
-<form action="validate_login.php" method="POST">
+  <form action="<?php echo $form_action; ?>" method="post">
     <label for="username">Username:</label>
     <input type="text" name="username" required><br>
 
